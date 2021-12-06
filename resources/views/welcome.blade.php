1e02b1b4b7c982b5ae74f3d4d9c5d596e5a3a950
artemDybin <tema.dybin@yandex.ru>
2021-12-06 11:12:26 +0300

    favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Копчёнки от Алёнки</title>
+        <link rel="icon" type="image/png" href="{{ asset('/favicon-32x32.png') }}">
         <script src="{{asset('js/app.js')}}" defer ></script>
         </style>
     </head>
